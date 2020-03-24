dce6e3e665b14d652dd9263bcf0e39c6ac710485
juliancastaeda <1277@holbertonschool.com>
2020-03-24 08:48:29 -0500

    upload file tsk 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
-#
+# Using Puppet, create a manifest that kills a process named killmenow.
+# pkill is a linux command, therefore the path from the origin is indicated
 exec { 'killmenow':
-command => '/usr/bin/pkill killmenow'
+    command => '/usr/bin/pkill killmenow';
 }

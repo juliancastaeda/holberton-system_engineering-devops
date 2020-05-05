15de74bad7f900e0f965895a3b457c55c14772b9
juliancastaeda <1277@holbertonschool.com>
2020-05-04 19:44:43 -0500

    load file 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+"""
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+
+    names_ = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(names_)
+    response_1 = requests.get(url)
+    jsonRes_1 = response_1.json()
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    response_2 = requests.get(url, params={'userId': sys.argv[1]}).json()
+    result = {}
+    completed = []
+
+    for compl in response_2:
+        if compl.get('userId') == int(names_):
+            new_json = {"task": compl.get('title'),
+                        "completed": compl.get('completed'),
+                        "username": jsonRes_1.get('username')}
+            completed.append(new_json)
+    result[names_] = completed
+
+    file = names_ + '.json'
+    with open(file, mode='w') as fil:
+        json.dump(result, fil)

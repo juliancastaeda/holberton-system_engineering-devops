87de8d6b7252c6fd16fb677c59502fea15b9bc64
juliancastaeda <1277@holbertonschool.com>
2020-05-04 20:06:23 -0500

    load file task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+"""
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+
+    url_1 = requests.get('https://jsonplaceholder.typicode.com/users/{}').json()
+    url_2 = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    res = {}
+    for key in url_1:
+        titles = []
+        for compl in url_2:
+            if compl.get('userId') == key.get('id'):
+                new_json = {"username": key.get('username'),
+                            "titles": compl.get('title'),
+                            "completed": compl.get('completed')}
+                titles.append(new_json)
+        res[key.get('id')] = title
+    file = 'todo_all_employees' + '.json'
+    with open(file, mode='w') as fil:
+        json.dump(res, fil)

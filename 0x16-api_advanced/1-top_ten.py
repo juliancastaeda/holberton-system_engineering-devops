07aca88466f3a7a42e146aaf025ccaea336760e4
juliancastaeda <1277@holbertonschool.com>
2020-05-07 18:40:01 -0500

    upload file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""
+"""rite a function that queries the Reddit API and
+   returns the number of subscribers (not active users,
+   total subscribers) for a given subreddit. If an invalid
+   subreddit is given, the function should return 0.
 """
 import requests
 
 
 def top_ten(subreddit):
+    """    """
     top = requests.get('https://www.reddit.com/r/{}/hot.json'.
                        format(subreddit),
-                       headers={"User-Agent": "Julian"},
+                       headers={"User-Agent": "Juliancastaneda"},
                        allow_redirects=False)
     info = top.json().get("data", {}).get("children", {})
     if top.status_code != 200:

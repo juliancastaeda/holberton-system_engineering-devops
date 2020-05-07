3902aea9a64595e22537e7220fdd80c6119e34e6
juliancastaeda <1277@holbertonschool.com>
2020-05-07 18:17:26 -0500

    upload file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     subscribers = requests.get('https://www.reddit.com/r/{}/about.json'.
                                format(subreddit),
-                               headers={"User-Agent": "Juliancastaneda"},
+                               headers={"User-Agent": "X-Modhash"},
                                allow_redirects=False)
     if subscribers.status_code != 200:
         return 0
